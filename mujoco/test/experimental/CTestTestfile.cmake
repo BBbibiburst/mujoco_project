1d68d5e3b4acadc70cac3021191b2175dd977c73
zmy <1683356703@qq.com>
2026-03-27 15:15:43 +0800

    change mujoco install

--- mujoco/test/experimental/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zmy/MyProject/mujoco/test/experimental
-# Build directory: /home/zmy/MyProject/mujoco/build/test/experimental
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
